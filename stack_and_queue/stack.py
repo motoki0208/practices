848b87c431900603faa7bf9e3841af705354ddf5
motoki0208 <38752601+motoki0208@users.noreply.github.com>
2020-02-27 22:26:27 +0900

    e1, e2 (#1)

---
+++ stack_and_queue/stack.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def pop(self):
+        """ スタックの一番上からデータを削除する
+        """
+        if self.top is None:
+            raise Exception
+        ret = self.top.data
+        self.top = self.top.next
+        return ret
+
+    def push(self, val):
+        """ スタックの一番上にvalをデータとしてもつNodeを追加する
+        """
+        node = Node(val)
+        node.next = self.top
+        self.top = node
+
+    def peek(self):
+        """ スタックの一番上のNodeのデータを返す
+        """
+        if self.top is None:
+            raise Exception
+        return self.top.data
+
+    @property
+    def is_empty(self):
+        """ スタックが空かどうかを返す
+        """
+        return self.top is None
